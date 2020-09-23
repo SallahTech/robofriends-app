4d22c75401d49ccf462ea5f321bbcab7de827d76
SallahTech <DonlyRlMrKtr@gmail.com>
2020-09-23 17:12:12 +0300

    Updates

---
+++ precache-manifest.4f9964f1c0761f69bc0a9c026987684f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71086ea7848bfb5af8e7e23595d30457",
+    "url": "/robofriends-app/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "08d6439c9be58d40038a",
+    "url": "/robofriends-app/static/css/main.10871c12.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends-app/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends-app/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08d6439c9be58d40038a",
+    "url": "/robofriends-app/static/js/main.1e88bafa.chunk.js"
+  },
+  {
+    "revision": "5a521ff99a4ea7b5b57c",
+    "url": "/robofriends-app/static/js/runtime-main.0a8f7cb7.js"
+  }
+]);
